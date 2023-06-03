a2cde4d34dae3bf38a1be7bfc154ff760c93ff22
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-03 18:25:24 +0200

    Modification de "DateTest.java", ajout de la méthode de test "estBissextile()";

--- src/test/java/modele/DateTest.java
+++ src/test/java/modele/DateTest.java
@@ -1,7 +1,19 @@
 package modele;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTest {
+    @Test
+    @Order(1)
+    void estBissextile() {
+        System.out.println("Test méthode estBissextile");
+        assertTrue(Date.estBissextile(2400));
+        assertTrue(Date.estBissextile(2016));
+        assertFalse(Date.estBissextile(2100));
+        assertFalse(Date.estBissextile(2017));
 
+    }
 }
