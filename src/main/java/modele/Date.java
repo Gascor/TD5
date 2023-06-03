f57d0abd32b0f1634f56a0e5c85755c788a5016c
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-03 19:03:54 +0200

    Modification de "Date.java", Complétion de la méthode "dernierJourDuMois()";

--- src/main/java/modele/Date.java
+++ src/main/java/modele/Date.java
@@ -31,6 +31,23 @@ public class Date {
      * @return int
      */
     public static int dernierJourDuMois(int parMois, int parAnnee){
-        return 0;
+        switch (parMois) {
+            case 1, 3, 5, 7, 8, 10, 12 -> {
+                return 31;
+            }
+            case 2 -> {
+                if (Date.estBissextile(parAnnee)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            }
+            case 4, 6, 9, 11 -> {
+                return 30;
+            }
+            default -> {
+                return -1;
+            }
+        }
     }
 }
