aae110c975328d017ee668d8a742693dd35f33d8
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-03 19:02:25 +0200

    Revert "Modification de "Date.java", ajout de l'entête de la méthode "dernierJourDuMois()";"
    This reverts commit 7e3ff8800bd114eb35b22113be41ce5acb0185ad.

--- src/test/java/modele/DateTest.java
+++ src/test/java/modele/DateTest.java
@@ -14,16 +14,6 @@ class DateTest {
         assertTrue(Date.estBissextile(2016));
         assertFalse(Date.estBissextile(2100));
         assertFalse(Date.estBissextile(2017));
-    }
-    @Test @Order (2)
-    void dernierJourDuMois() {
-        System.out.println("Test méthode dernierJourDuMois");
-        assertEquals(31,Date.dernierJourDuMois(1,2017));
-        assertEquals(30,Date.dernierJourDuMois(4,2017));
-        assertEquals(28,Date.dernierJourDuMois(2,2017));
-        assertEquals(31,Date.dernierJourDuMois(1,2016));
-        assertEquals(30,Date.dernierJourDuMois(4,2016));
-        assertEquals(29,Date.dernierJourDuMois(2,2016));
-        assertEquals(-1,Date.dernierJourDuMois(13,2100));
+
     }
 }
