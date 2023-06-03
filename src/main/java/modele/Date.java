97b1b45ed305cab0796cd34623d2be8e64318829
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-03 18:23:04 +0200

    Modification de "Date.java", ajout de son constructeur "Date()"; Modification de "Date.java", ajout des champs;

--- src/main/java/modele/Date.java
+++ src/main/java/modele/Date.java
@@ -1,4 +1,17 @@
 package modele;
 
 public class Date {
+    protected int chJour, chMois, chAnnee;
+
+    /**
+     * Date class constructor for one argument.
+     * Creates a date corresponding to the first of january of a given year.
+     *
+     * @param parAnnee The year of the date.
+     */
+    public Date(int parAnnee) {
+        chJour = 1;
+        chMois = 1;
+        chAnnee = parAnnee;
+    }
 }
