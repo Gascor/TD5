305c151a8c141fef66561a352b109589a64a1bb2
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-03 18:15:31 +0200

    Netoyage du projet...

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.td5 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.td5 to javafx.fxml;
     exports com.example.td5;
 }
